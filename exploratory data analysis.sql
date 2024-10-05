9e46ba3cccaa77b8470dbf1a37f5fc8fd7e28201
laurachidinma735 <123868260+laurachidinma735@users.noreply.github.com>
2024-10-05 15:52:27 +0100

    Add files via upload

---
+++ exploratory data analysis.sql
@@ -0,0 +1,81 @@
+-- Exploratory Data Analysis --
+
+SELECT *
+FROM layoffs_stage2;
+
+SELECT *
+FROM layoffs_stage2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_stage2;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT *
+FROM layoffs_stage2;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY company
+ORDER BY 2 DESC;
+
+
+SELECT SUBSTRING(`date`, 1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_stage2
+WHERE SUBSTRING(`date`, 1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`, 1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_stage2
+WHERE SUBSTRING(`date`, 1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`, total_off,
+SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY company,YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_stage2
+GROUP BY company,YEAR(`date`)
+), Company_Year_Rank AS
+(SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+ )
+ SELECT *
+ FROM Company_Year_Rank
+ WHERE Ranking <= 5;
+ 
+
